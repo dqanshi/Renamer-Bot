e0eb4cd10445f2d77eb68bcee795e5aa7432fa65
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2022-02-03 21:18:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if bool(os.environ.get("WEBHOOK", False)):
 
-    from sample_config import Config
-else:
-    from config import Config
+from sample_config import Config
 
 from pyrogram import Client as Clinton
 
